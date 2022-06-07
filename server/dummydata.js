2fbc4e7bb7d1c3f6f9d025ecdb671e31447a98a1
Nico Pasqualini <pasqualinin9@gmail.com>
2022-06-07 15:05:02 +1000

    initializing server and first queries

---
+++ server/dummydata.js
@@ -0,0 +1,79 @@
+// Projects
+const projects = [
+    {
+      id: '1',
+      clientId: '1',
+      name: 'marketing Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '2',
+      clientId: '2',
+      name: 'Finance App',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '3',
+      clientId: '3',
+      name: 'Small Project',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '4',
+      clientId: '4',
+      name: 'Java project',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'Done',
+    },
+    {
+      id: '5',
+      clientId: '5',
+      name: 'Cars Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+  ];
+  
+  // Clients
+  const clients = [
+    {
+      id: '1',
+      name: 'Mark Reed',
+      email: 'mark@gmail.com',
+      phone: '343-567-4333',
+    },
+    {
+      id: '2',
+      name: 'Larry Wills',
+      email: 'larry@gmail.com',
+      phone: '223-567-3322',
+    },
+    {
+      id: '3',
+      name: 'Julia Edson',
+      email: 'julia@gmail.com',
+      phone: '324-331-4333',
+    },
+    {
+      id: '4',
+      name: 'Steve White',
+      email: 'steve@gmail.com',
+      phone: '344-562-6787',
+    },
+    {
+      id: '5',
+      name: 'Bobby Gray',
+      email: 'bobby@gmail.com',
+      phone: '321-468-8887',
+    },
+  ];
+  
+  module.exports = { projects, clients };
---
+++ server/schema/schema.js
@@ -0,0 +1,65 @@
+const {projects, clients} = require ('../dummydata.js')
+
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList} = require ('graphql')
+
+//client type
+const ClientType = new GraphQLObjectType({
+    name:"Client",
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString},
+        email:{type:GraphQLString},
+        phone:{type:GraphQLString},
+    })
+})
+
+//project type
+const ProjectType = new GraphQLObjectType({
+    name:"Project",
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString},
+        description:{type:GraphQLString},
+        status:{type:GraphQLString},
+    })
+})
+
+//DEFINING OUR QUERIES
+const RootQuery = new GraphQLObjectType({
+    name:"RootQueryType",
+    fields:{
+        client:{
+            type:ClientType,
+            // we pass the id as args to pick a client
+            args:{id:{type:GraphQLID}},
+            resolve(parent, args){
+                return clients.find(client =>client.id === args.id)
+            }
+        },
+        clients:{
+            type: new GraphQLList(ClientType),
+            resolve(parent,args){
+                return clients //return the full arrays of clients
+            }
+        },
+        project:{
+            type:ProjectType,
+            // we pass the id as args to pick a client
+            args:{id:{type:GraphQLID}},
+            resolve(parent, args){
+                return projects.find(project =>project.id === args.id)
+            }
+        },
+        projects:{
+            type: new GraphQLList(ProjectType),
+            resolve(parent,args){
+                return projects
+            }
+        }
+    }
+})
+
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+})
